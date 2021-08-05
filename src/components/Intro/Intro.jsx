73f6a56d02ee8b4a14970a3b5000f14617279534
Jfig27 <79497737+Jfig27@users.noreply.github.com>
2021-08-05 00:15:58 -0400

    intro added

--- src/components/Intro/Intro.jsx
+++ src/components/Intro/Intro.jsx
@@ -1,9 +1,24 @@
 import './intro.scss'
+import ArrowDownwardIcon from '@material-ui/icons/ArrowDownward';
 
 export default function Intro() {
     return (
         <div className="intro" id="intro">
-            Intro
+            <div className="left">
+                <div className="imgContainer">
+                    <img src="assets/self.png" alt="my pic" />
+                </div>
+            </div>
+            <div className="right">
+                <div className="wrapper">
+                    <h2>Hey there, I'm</h2>
+                    <h1>Joshua Figuereo</h1>
+                    <h3>Web Developer</h3>
+                </div>
+                <a href="#portfolio">
+                    <ArrowDownwardIcon style={{fontSize: 90}}/>
+                </a>
+            </div>
         </div>
     )
 }
