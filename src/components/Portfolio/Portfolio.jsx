b522980d451c70418a5c7efca49b1d8e94d05e4b
Jfig27 <79497737+Jfig27@users.noreply.github.com>
2021-08-05 01:35:31 -0400

    wip portfolio page added

--- src/components/Portfolio/Portfolio.jsx
+++ src/components/Portfolio/Portfolio.jsx
@@ -3,7 +3,22 @@ import './portfolio.scss'
 export default function Portfolio() {
     return (
         <div className="portfolio" id="portfolio">
-            portfolio
+            <h1>Portfolio</h1>
+            <ul>
+                <li className="active">Featured</li>
+                <li >Web App - <span >Coming Soon</span></li>
+                <li >Design - <span >Coming Soon</span></li>
+            </ul>
+            <div className="container">
+                <div className="item">
+                    <img src="assets/f76.png" alt="" />
+                    <h3>F76 Inventory Management app</h3>
+                </div>
+                <div className="item">
+                    <img src="assets/MARVEL.png" alt="" />
+                    <h3>Marvel Hero Information app</h3>
+                </div>
+            </div>
         </div>
     )
 }
